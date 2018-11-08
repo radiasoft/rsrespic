6745029c1de01b027db5f446442fda3a287158cf
j-edelen <jedelen@radiasoft.net>
2018-11-08 15:34:33 -0700

    Updating particle class to save opal distributions.

--- development/particles.py
+++ development/particles.py
@@ -155,7 +155,22 @@ class particles_2D_delta:
 		return np.ones(k_matrix.shape)
 
 
+	def write_opal_distribution(self, z_extent = 1.0):
+		## This will write a file that can be loaded into opal with an extent of 1m 
 
+		## Convert units on particles
+		x_out = self.x / 100.
+		y_out = self.y / 100.
+		z_out = np.random.uniform(0, z_extent, self.N)
+		px_out = self.px / (self.m_0 * c * self.weight)
+		py_out = self.py / (self.m_0 * c * self.weight)
+		pz_out = self.pz / (self.m_0 * c * self.weight)
+
+		particle_array = np.asarray([x_out, px_out, y_out, py_out, z_out, pz_out])
+
+		np.savetxt('opal_input_distribution.txt', particle_array, delimiter = '\t')
+
+		return
 
 class particles_2D_tent:
 
