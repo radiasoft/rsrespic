12de31f64edfdec39b96bc7b5b43c20f1f6960c6
j-edelen <jedelen@radiasoft.net>
2019-03-19 15:54:12 -0600

    Updating respic, additonal diagnostics and changes to the examples

--- examples/respic_fodo.py
+++ examples/respic_fodo.py
@@ -1,4 +1,67 @@
 
+class octo_respic: 
+    def __init__(self, s0 = 0):
+        ## Drit defined in meters and converted to CM for respic
+        self.s0 = s0
+
+
+    def drift(self, maps, fields, particles, diagnostics, s, L = 0):
+    
+        maps.drift(particles, ds = L/2.)
+        maps.space_charge_kick_2D(fields, particles, ds = L)
+        maps.drift(particles, ds = L/2.)
+        s += L
+        diagnostics.update(s, particles)
+    
+        return s
+
+    def quad(self, maps, fields, particles, diagnostics, s, L = 0., k = 0):
+
+        maps.thin_quad(fields, particles, ds = L/4., kappa = k)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L/4.)
+        maps.thin_quad(fields, particles, ds = L/4., kappa = k)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L/4.)
+        maps.thin_quad(fields, particles, ds = L/4., kappa = k)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L/4.)
+        maps.thin_quad(fields, particles, ds = L/4., kappa = k)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L/4.)
+
+        #s += L * 100.
+        #diagnostics.update(s, particles) 
+        
+        return s
+
+    def drift_1(self, maps, fields, particles, diagnostics, s):
+        L_d1 = 0.5 # units of meters
+        L_d2 = 1.0 # units of meters
+
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L_d1 * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L_d1 * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L_d1 * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L_d2 / 2. * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L_d2 / 2. * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L_d1 * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = L_d1 * 100.)
+        
+        return s
+
+
+    def one_turn_map(self, maps, fields, particles, diagnostics, s):
+        L_quad = 0.1
+
+        s = self.quad(maps, fields, particles, diagnostics, s, L = L_quad / 2. * 100., k = 4.0)
+
+        s = self.drift_1(maps, fields, particles, diagnostics, s)
+
+        s = self.quad(maps, fields, particles, diagnostics, s, L = L_quad * 100., k = -4.0)
+        
+        s = self.drift_1(maps, fields, particles, diagnostics, s)
+        
+        s = self.quad(maps, fields, particles, diagnostics, s, L = L_quad / 2.* 100., k = 4.0)
+
+        return s
+
+
 
 class fodo_cell:
 
@@ -111,6 +174,69 @@ class fodo_cell2:
         
         return s
 
+
+    def one_turn_map(self, maps, fields, particles, diagnostics, s):
+        
+        s = self.drift_1(maps, fields, particles, diagnostics, s)
+
+        s = self.quad(maps, fields, particles, diagnostics, s, L = 1.0e-6, k = self.k_array[0])
+        
+        s = self.drift_1(maps, fields, particles, diagnostics, s)
+        
+        s = self.quad(maps, fields, particles, diagnostics, s, L = 1.0e-6, k = self.k_array[1])
+
+        s = self.drift_1(maps, fields, particles, diagnostics, s)
+        
+        s = self.quad(maps, fields, particles, diagnostics, s, L = 1.0e-6, k = self.k_array[2])
+
+        s = self.drift_1(maps, fields, particles, diagnostics, s)
+        
+        s = self.quad(maps, fields, particles, diagnostics, s, L = 1.0e-6, k = self.k_array[3])
+
+        s = self.drift_1(maps, fields, particles, diagnostics, s)
+
+        return s
+
+
+class fodo_cell_2:
+
+    def __init__(self, L = 0.1, s0 = 0, k_array = [0, 0, 0, 0]):
+        ## Drit defined in meters and converted to CM for respic
+        self.s0 = s0
+        self.L = L 
+        self.k_array = k_array
+
+
+    def drift(self, maps, fields, particles, diagnostics, s, L = 0):
+    
+        maps.drift(particles, ds = L)
+        s += L
+        diagnostics.update(s, particles)
+    
+        return s
+
+    def quad(self, maps, fields, particles, diagnostics, s, L = 1.0e-6, k = 0):
+
+        maps.thin_quad(fields, particles, ds = L * 100., kappa = k)
+        s += L * 100.
+        diagnostics.update(s, particles) 
+        
+        return s
+
+    def drift_1(self, maps, fields, particles, diagnostics, s):
+        
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        s = self.drift(maps, fields, particles, diagnostics, s, L = self.L * 100.)
+        
+        return s
 
     def one_turn_map(self, maps, fields, particles, diagnostics, s):
         
--- rsrespic/utilities/diagnostics.py
+++ rsrespic/utilities/diagnostics.py
@@ -1,6 +1,32 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import h5py as h5
+
+class particle_dumper: 
+
+    def __init__(self, file_name = 'particle_coordinates.h5')
+    
+        self.fn = file_name
+        self.hf = h5.File(self.fn, 'w')
+
+    def dump(self, step, particles):
+        data_set_name = 'step_' + str(step)
+
+        x = particles.x / 100.
+        y = particles.y / 100.
+        xp = particles.px / particles.pz
+        yp = particles.py / particles.pz
+        
+        array_data = np.column_stack([x, xp, y, yp])
+        
+        self.hf.create_dataset(data_set_name, array_data)
+        return
+
+    def close(self):
+        self.hf.close()
+        return
+        
 
 
 class bunch_statistics:
